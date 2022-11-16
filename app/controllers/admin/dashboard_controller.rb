9870dad214dcb5adafc9c3f181a7353eacd2181c
colspen <spencerlewiscole@gmail.com>
2022-11-16 20:22:40 +0000

    setup UI for Admin Dash using ActiveRecord queries via controller

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,5 +2,7 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with :name => ENV['ADMIN_USERNAME'], :password => ENV['ADMIN_PASSWORD']
   
   def show
+    @total_product_count = Product.count
+    @total_category_count = Category.count
   end
 end
