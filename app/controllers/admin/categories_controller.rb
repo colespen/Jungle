9a728ff3daea038ede7fbc968d35bb7219afbbaa
colspen <spencerlewiscole@gmail.com>
2022-11-17 02:20:04 +0000

    complete Admin/categories functionality and UI

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -2,11 +2,7 @@ class Admin::CategoriesController < ApplicationController
   http_basic_authenticate_with :name => ENV['ADMIN_USERNAME'], :password => ENV['ADMIN_PASSWORD']
 
   def index
-    
     @categories = Category.order(id: :desc).all
-    # @category = Category.find(params[:id])
-    # @products = @category.products.order(created_at: :desc)
-    # @products_per_category = 
   end
 
   def new
