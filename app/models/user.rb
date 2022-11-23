30754316906e2c39a21fb5743288388922297157
colspen <spencerlewiscole@gmail.com>
2022-11-23 00:06:32 +0000

    fix checkout order bug when no user session

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
 
   def self.authenticate_with_credentials(email, password)
+
     !email ? nil : email_no_space_lower = email.strip.downcase
 
     user = User.find_by_email(email_no_space_lower)
@@ -10,7 +11,8 @@ class User < ApplicationRecord
 
   has_secure_password
 
-  validates :password, length: { minimum: 8 }
+  validates :first_name, presence: true 
+  validates :password, presence: true, length: { minimum: 8 }
   validates :email, presence: true, uniqueness: {case_sensitive: false} 
 
   before_save do 
